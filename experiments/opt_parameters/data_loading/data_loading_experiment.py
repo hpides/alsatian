173330190776a13f02c3f064326b22a0a3eb9e44
Nils Strassenburg <nils.strassenburg@hpi.de>
2024-02-29 20:06:31 +0100

    data loading experiment with two different dataset types

--- experiments/opt_parameters/data_loading/data_loading_experiment.py
+++ experiments/opt_parameters/data_loading/data_loading_experiment.py
@@ -4,6 +4,8 @@ import time
 
 import torch
 
+from custom.data_loaders.custom_image_folder import CustomImageFolder
+from custom.dataset_transfroms import imagenet_inference_transform
 from experiments.opt_parameters.data_loading.experiment_args import ExpArgs
 from global_utils.constants import LOAD_DATA
 from global_utils.dummy_dataset import DummyDataset
@@ -22,47 +24,56 @@ if __name__ == '__main__':
     nums_workers = [1, 2, 4, 8, 32, 48, 64]
     sleeps = [None, 2]
 
-    dataset_size = 1024 * 10
-    data_set = DummyDataset(1024 * 10, (3, 224, 224), (1,), exp_args.dummy_input_dir, saved_items=dataset_size)
 
-    for batch_size in batch_sizes:
-        for num_workers in nums_workers:
-            for sleep in sleeps:
-                print(f"{batch_size} - {num_workers}")
-                exp_args.batch_size = batch_size
-                exp_args.data_workers = num_workers
 
-                data_loader = torch.utils.data.DataLoader(data_set, batch_size=exp_args.batch_size,
-                                                          shuffle=False,
-                                                          num_workers=exp_args.data_workers)
+    for dataset_type in ['imagenette', 'preprocessed_ssd']:
 
-                measurements = {}
-                measurements[LOAD_DATA] = []
+        if dataset_type == 'preprocessed_ssd':
+            dataset_size = 1024 * 10
+            data_set = DummyDataset(1024 * 10, (3, 224, 224), (1,), exp_args.dummy_input_dir, saved_items=dataset_size)
+        elif dataset_type == 'imagenette':
+            data_set = CustomImageFolder(os.path.join(exp_args.dataset_path, 'train'),
+                                         imagenet_inference_transform)
 
-                end_to_end_start = time.perf_counter()
+        for batch_size in batch_sizes:
+            for num_workers in nums_workers:
+                for sleep in sleeps:
+                    print(f"{batch_size} - {num_workers}")
+                    exp_args.batch_size = batch_size
+                    exp_args.data_workers = num_workers
 
-                with torch.no_grad():
-                    start = time.perf_counter()
 
-                    for i, (inputs, l) in enumerate(data_loader):
-                        # measure data loading time
-                        measurements[LOAD_DATA].append(time.perf_counter() - start)
+                    data_loader = torch.utils.data.DataLoader(data_set, batch_size=exp_args.batch_size,
+                                                              shuffle=False,
+                                                              num_workers=exp_args.data_workers)
 
-                        # simulate the time it takes to compute inference
-                        if sleep is not None:
-                            time.sleep(sleep)
+                    measurements = {}
+                    measurements[LOAD_DATA] = []
 
+                    end_to_end_start = time.perf_counter()
+
+                    with torch.no_grad():
                         start = time.perf_counter()
 
-                        if i > 9:
-                            break
+                        for i, (inputs, l) in enumerate(data_loader):
+                            # measure data loading time
+                            measurements[LOAD_DATA].append(time.perf_counter() - start)
 
-                time.sleep(2)
+                            # simulate the time it takes to compute inference
+                            if sleep is not None:
+                                time.sleep(sleep)
 
-                exp_id = f'data-loading-exp-params-des-gpu-{num_workers}-batch_size-{batch_size}-sleep-{sleep}'
-                write_measurements_and_args_to_json_file(
-                    measurements=measurements,
-                    args=exp_args.get_dict(),
-                    dir_path=exp_args.result_dir,
-                    file_id=exp_id
-                )
+                            start = time.perf_counter()
+
+                            if i > 9:
+                                break
+
+                    time.sleep(2)
+
+                    exp_id = f'data-loading-exp-params-des-gpu-{num_workers}-batch_size-{batch_size}-sleep-{sleep}-data-{dataset_type}'
+                    write_measurements_and_args_to_json_file(
+                        measurements=measurements,
+                        args=exp_args.get_dict(),
+                        dir_path=exp_args.result_dir,
+                        file_id=exp_id
+                    )
