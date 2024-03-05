080996e3f8b9ea5388437b3b74573dea7a1bdf1d
Nils Strassenburg <nils.strassenburg@hpi.de>
2024-03-05 11:06:49 +0100

    started setting up a simple BERT pipeline

--- experiments/bert_sentiment/dummy_data.py
+++ experiments/bert_sentiment/dummy_data.py
@@ -1,12 +1,7 @@
-import os
-import statistics
 import time
 
 import torch
-from torch.utils.data import DataLoader, TensorDataset
-from tqdm import tqdm
-from transformers import BertTokenizer, BertForSequenceClassification
-
+from transformers import BertForSequenceClassification
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -29,7 +24,7 @@ if __name__ == '__main__':
     end_to_end_start = time.time()
     batch_times = []
     with torch.no_grad():
-        for i in range (num_batches):
+        for i in range(num_batches):
             # start = time.time()
             starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
             starter.record()
@@ -37,7 +32,7 @@ if __name__ == '__main__':
             ender.record()
             torch.cuda.synchronize()  # WAIT FOR GPU SYNC
             elapsed = starter.elapsed_time(ender)
-            batch_times.append(elapsed * 10**-3)
+            batch_times.append(elapsed * 10 ** -3)
             # time.sleep(2)
 
     end_to_end_end = time.time()
@@ -45,4 +40,3 @@ if __name__ == '__main__':
     print(f'sum: {sum(batch_times)}')
     print(batch_size * num_batches / sum(batch_times) * 2.35)
     print(batch_times)
-
