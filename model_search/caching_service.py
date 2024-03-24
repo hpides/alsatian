1f9542827cd17dee9ecda77890d4fe06e11aa5e3
Nils Strassenburg <nils.strassenburg@hpi.de>
2024-03-24 14:47:01 +0100

    implement extended version of caching service + tests

--- model_search/caching_service.py
+++ model_search/caching_service.py
@@ -1,3 +1,5 @@
+import os
+
 import torch.nn.functional
 from torch import Tensor
 
@@ -5,42 +7,137 @@ from global_utils.benchmark_util import CPU
 from global_utils.constants import CUDA
 
 
-class CachingService:
+class TensorCachingService:
 
-    def __init__(self):
-        self.gpu_cache = {}
-        self.cpu_cache = {}
-        self.ssd_cache = {}
+    def __init__(self, persistent_path: os.path):
+        """
+        :param persistent_path: the path on the file system used to cache tensors on persistent storage
+        """
+        self.ssd_path = persistent_path
+        self._gpu_cache = {}
+        self._cpu_cache = {}
+        self._persistent_cache = {}
+        self._delete_later = []
+
+    def __del__(self):
+        self.remove_referenced_files()
+        self.remove_unreferenced_files()
+
+    def remove_referenced_files(self):
+        for file_id in self._persistent_cache.keys():
+            path = self._get_path(file_id)
+            self._delete_file(path)
+
+    def remove_unreferenced_files(self):
+        cached_files = set(self._gpu_cache.keys()) | set(self._cpu_cache.keys()) | set(self._persistent_cache.keys())
+        for filename in os.listdir(self.ssd_path):
+            file_id, file_extension = os.path.splitext(filename)
+            if file_extension == ".pt" and file_id not in cached_files:
+                path = os.path.join(self.ssd_path, filename)
+                self._delete_file(path)
+
+    def get_tensor(self, _id):
+        if _id in self._gpu_cache:
+            return self._gpu_cache[_id]
+        elif _id in self._cpu_cache:
+            return self._cpu_cache[_id]
+        elif _id in self._persistent_cache:
+            return torch.load(self._get_path(_id))
+        else:
+            raise KeyError(f'{_id} does not exist in any cache')
+
+    def remove_tensor(self, _id, remove_immediately=False):
+        if _id in self._gpu_cache:
+            del self._gpu_cache[_id]
+        elif _id in self._cpu_cache:
+            del self._cpu_cache[_id]
+        elif _id in self._persistent_cache:
+            if remove_immediately:
+                path = self._get_path(_id)
+                self._delete_file(path)
+            del self._persistent_cache[_id]
+        else:
+            raise KeyError(f'{_id} does not exist in any cache')
 
     def cache_on_gpu(self, _id, data: Tensor):
+        self._check_id_not_exists(_id)
         if data.is_cuda:
-            self.gpu_cache[_id] = data
+            self._gpu_cache[_id] = data
         else:
-            self.gpu_cache[_id] = data.to(CUDA)
+            self._gpu_cache[_id] = data.to(CUDA)
 
     def cache_on_cpu(self, _id, data: Tensor):
+        self._check_id_not_exists(_id)
         if data.is_cuda:
-            self.cpu_cache[_id] = data.to(CPU)
+            self._cpu_cache[_id] = data.to(CPU)
         else:
-            self.cpu_cache[_id] = data
+            self._cpu_cache[_id] = data
 
-    def cache_on_ssd(self, _id, data: Tensor):
-        raise NotImplementedError
+    def cache_persistent(self, _id, data: Tensor):
+        self._check_id_not_exists(_id)
+        if data.is_cuda:
+            data = data.to(CPU)
+        path = self._get_path(_id)
+        torch.save(data, path)
+        self._persistent_cache[_id] = path
+
+    def _get_path(self, _id):
+        return os.path.join(self.ssd_path, f'{_id}.pt')
 
     def move_to_cpu(self, _id):
-        self.cpu_cache[_id] = self.gpu_cache[_id].to(CPU)
-        self.gpu_cache.pop(_id)
-        torch.cuda.empty_cache()
+        if _id in self._gpu_cache:
+            data = self._gpu_cache.pop(_id).to(CPU)
+        elif _id in self._persistent_cache:
+            data = self._persistent_cache.pop(_id)
+        else:
+            raise KeyError(f'{_id} does not exists')
+
+        self.cache_on_cpu(_id, data)
 
     def move_to_gpu(self, _id):
-        self.gpu_cache[_id] = self.cpu_cache[_id].to(CUDA)
+        if _id in self._persistent_cache:
+            data = self._persistent_cache.pop(_id)
+        elif _id in self._cpu_cache:
+            data = self._cpu_cache.pop(_id)
+        else:
+            raise KeyError(f'{_id} does not exists')
 
-    def move_to_ssd(self, _id):
-        raise NotImplementedError
+        self.cache_on_gpu(_id, data)
+
+    def move_to_persistent(self, _id):
+        if _id in self._gpu_cache:
+            data = self._gpu_cache.pop(_id).to(CPU)
+        elif _id in self._cpu_cache:
+            data = self._cpu_cache.pop(_id)
+        else:
+            raise KeyError(f'{_id} does not exists')
+
+        self.cache_persistent(_id, data)
+
+    def _delete_file(self, file_path):
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"File '{file_path}' deleted successfully.")
+        else:
+            print(f"File '{file_path}' does not exist.")
+
+    def id_exists(self, _id):
+        if _id in self._gpu_cache:
+            return True
+        elif _id in self._cpu_cache:
+            return True
+        elif _id in self._persistent_cache:
+            return True
+        else:
+            return False
+
+    def _check_id_not_exists(self, _id):
+        if self.id_exists(_id):
+            raise KeyError(f'{_id} already cached')
 
 
 if __name__ == '__main__':
-    cs = CachingService()
+    cs = TensorCachingService('./')
     for i in range(5):
         t = torch.rand(1024, 3, 224, 224)
         cs.cache_on_gpu(str(i), t)
---
+++ tests/model_search/test_caching_service.py
@@ -0,0 +1,80 @@
+import os
+import unittest
+
+import torch
+
+from global_utils.constants import CUDA
+from model_search.caching_service import TensorCachingService
+
+ID_1 = 'id1'
+
+
+class TestTensorCachingService(unittest.TestCase):
+    def setUp(self):
+        self.persistent_path = "./test_data"
+        os.makedirs(self.persistent_path, exist_ok=True)
+        self.tensor1 = torch.tensor([1, 2, 3])
+        self.tensor2 = torch.tensor([4, 5, 6])
+        self.tensor3 = torch.tensor([7, 8, 9])
+
+        self.service = TensorCachingService(self.persistent_path)
+
+    def tearDown(self):
+        for file in os.listdir(self.persistent_path):
+            os.remove(os.path.join(self.persistent_path, file))
+        os.rmdir(self.persistent_path)
+
+    def test_cache_on_gpu(self):
+        self.service.cache_on_gpu(ID_1, self.tensor1)
+        self.assertTrue(ID_1 in self.service._gpu_cache)
+        result = self.service.get_tensor(ID_1)
+        tensor_1_cuda = self.tensor1.to(CUDA)
+        self.assertTrue(torch.equal(result, tensor_1_cuda))
+
+    def test_cache_on_cpu(self):
+        self.service.cache_on_cpu(ID_1, self.tensor1)
+        self.assertTrue(ID_1 in self.service._cpu_cache)
+        result = self.service.get_tensor(ID_1)
+        self.assertTrue(torch.equal(result, self.tensor1))
+
+    def test_cache_persistent(self):
+        self.service.cache_persistent(ID_1, self.tensor1)
+        self.assertTrue(ID_1 in self.service._persistent_cache)
+        result = self.service.get_tensor(ID_1)
+        self.assertTrue(torch.equal(result, self.tensor1))
+
+    def test_move_gpu_to_cpu(self):
+        self.service.cache_on_gpu(ID_1, self.tensor1)
+        self.service.move_to_cpu(ID_1)
+        self.assertFalse(ID_1 in self.service._gpu_cache)
+        self.assertTrue(ID_1 in self.service._cpu_cache)
+        result = self.service.get_tensor(ID_1)
+        self.assertTrue(torch.equal(result, self.tensor1))
+
+    def test_move_gpu_to_persistent(self):
+        self.service.cache_on_gpu(ID_1, self.tensor1)
+        self.service.move_to_persistent(ID_1)
+        self.assertFalse(ID_1 in self.service._gpu_cache)
+        self.assertTrue(ID_1 in self.service._persistent_cache)
+        result = self.service.get_tensor(ID_1)
+        self.assertTrue(torch.equal(result, self.tensor1))
+
+    def test_move_cpu_to_gpu(self):
+        self.service.cache_on_cpu(ID_1, self.tensor1)
+        self.service.move_to_gpu(ID_1)
+        self.assertFalse(ID_1 in self.service._cpu_cache)
+        self.assertTrue(ID_1 in self.service._gpu_cache)
+        result = self.service.get_tensor(ID_1)
+        tensor_1_cuda = self.tensor1.to(CUDA)
+        self.assertTrue(torch.equal(result, tensor_1_cuda))
+
+    def test_add_existing_id(self):
+        self.service.cache_on_cpu(ID_1, self.tensor1)
+        self.service.move_to_gpu(ID_1)
+
+        with self.assertRaises(KeyError):
+            self.service.cache_on_cpu(ID_1, self.tensor2)
+
+    def test_get_tensor_non_existing(self):
+        with self.assertRaises(KeyError):
+            self.service.get_tensor('non_existing_id')
