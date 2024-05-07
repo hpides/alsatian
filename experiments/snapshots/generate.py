e08c75b6dbbc704718489ae0d85ee22819bf9489
Nils Strassenburg <nils.strassenburg@hpi.de>
2024-05-07 11:19:13 +0200

    generate snapshots with hardcoded retrain indexes

--- experiments/snapshots/generate.py
+++ experiments/snapshots/generate.py
@@ -1,11 +1,3 @@
-# % - start with models_set = {pretrained_model}
-# % - while have to generate more models
-# % 	- randomly pick model from model_set
-# % 	- draw n from distribution (and round to integer)
-# % 	- generate new snapshot by
-# % 		- copy first (num_layers - n) layers
-# % 		- adjust rest of layers
-# % 	- add new snapshots to model_set
 import random
 from enum import Enum
 
@@ -17,19 +9,32 @@ from global_utils.model_operations import split_model_in_two
 
 
 class RetrainDistribution(Enum):
-    RANDOM = 1
-    TOP_LAYERS = 2
-    TWENTY_FIVE_PERCENT = 3
-    FIFTY_PERCENT = 4
+    HARD_CODED = 1
+    RANDOM = 2
+    TOP_LAYERS = 3
+    TWENTY_FIVE_PERCENT = 4
+    FIFTY_PERCENT = 5
+
+
+def _num_retrained_layers(max_number, distribution: RetrainDistribution) -> int:
+    if distribution == RetrainDistribution.RANDOM:
+        return random.randint(0, max_number)
+    elif distribution == RetrainDistribution.TOP_LAYERS:
+        raise NotImplementedError
+    elif distribution == RetrainDistribution.TWENTY_FIVE_PERCENT:
+        raise NotImplementedError
+    elif distribution == RetrainDistribution.FIFTY_PERCENT:
+        raise NotImplementedError
+    else:
+        raise ValueError(f"invalid distribution: {distribution}")
 
 
 def _adjust_model_randomly(architecture_name: str, base_model: torch.nn.Sequential,
-                           distribution: RetrainDistribution) -> torch.nn.Module:
-    # draw the number of retrained layers form a distribution
-    # TODO
-    num_retrained = 3
+                           distribution: RetrainDistribution, retrain_idx=None) -> torch.nn.Module:
+    if retrain_idx is None:
+        retrain_idx = _num_retrained_layers(len(base_model), distribution)
 
-    keep_layers = len(base_model) - num_retrained
+    keep_layers = len(base_model) - retrain_idx
     _, (_, second_layer_names) = split_model_in_two(base_model, keep_layers, include_layer_names=True)
     copied_weights = base_model.state_dict()
     for parm_key in second_layer_names:
@@ -41,18 +46,27 @@ def _adjust_model_randomly(architecture_name: str, base_model: torch.nn.Sequenti
     return new_model
 
 
-def generate_snapshots(architecture_name: str, num_models: int, distribution: RetrainDistribution) -> [torch.nn.Module]:
+def generate_snapshots(architecture_name: str, num_models: int, distribution: RetrainDistribution,
+                       retrain_idxs=None) -> [torch.nn.Module]:
+    if distribution == RetrainDistribution.HARD_CODED:
+        assert retrain_idxs is not None
+
     # always start with a model pretrained on Imagenet
     pre_trained = initialize_model(architecture_name, pretrained=True, features_only=True)
     generated_models = [pre_trained]
-    for _ in range(num_models - 1):
+    for i in range(num_models - 1):
         base_model = random.choice(generated_models)
-        new_model = _adjust_model_randomly(architecture_name, base_model, distribution)
+        if retrain_idxs:
+            new_model = _adjust_model_randomly(architecture_name, base_model, distribution, retrain_idxs[i])
+        else:
+            new_model = _adjust_model_randomly(architecture_name, base_model, distribution)
         generated_models.append(new_model)
 
     return generated_models
 
 
 if __name__ == '__main__':
-    pre_trained = initialize_model(RESNET_18, pretrained=True, features_only=True)
-    _adjust_model_randomly(RESNET_18, pre_trained, None)
+    snaps = generate_snapshots(RESNET_18, 4, RetrainDistribution.HARD_CODED, [5, 7, 9])
+    print('test')
+
+
