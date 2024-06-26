038629820db4461366dd2239cb7e74dfbb66b12c
Nils Strassenburg <nils.strassenburg@hpi.de>
2024-06-26 15:39:58 +0200

    fix path

--- experiments/model_search/model_search_exp.py
+++ experiments/model_search/model_search_exp.py
@@ -61,7 +61,8 @@ def run_model_search(exp_args: ExpArgs):
 
     model_snapshots, model_store = get_snapshots(exp_args.snapshot_set_string, exp_args.num_models,
                                                  exp_args.distribution, exp_args.base_snapshot_save_path)
-    model_store.add_output_sizes_to_rich_snapshots('../model_resource_info/outputs/layer_output_infos.json')
+    layer_output_info = os.path.abspath('../model_resource_info/outputs/layer_output_infos.json')
+    model_store.add_output_sizes_to_rich_snapshots(layer_output_info)
 
     # after generating the snapshots make sure they are not in the caches
     clear_caches_and_check_io_limit()
