456011e290e233d113fe2923ff55170d836a2339
Nils Strassenburg <nils.strassenburg@hpi.de>
2025-01-04 09:46:42 +0100

    fix long file name

--- experiments/main_experiments/model_search/run_experiment_set_hf_snapshots.py
+++ experiments/main_experiments/model_search/run_experiment_set_hf_snapshots.py
@@ -12,7 +12,7 @@ from experiments.main_experiments.model_search.experiment_args import ExpArgs, _
 from experiments.main_experiments.model_search.model_search_exp import run_model_search
 from experiments.main_experiments.prevent_caching.watch_utils import LIMIT_IO
 from experiments.main_experiments.snapshots.hugging_face.init_hf_models import MICROSOFT_RESNET_152, \
-    MICROSOFT_RESNET_18
+    MICROSOFT_RESNET_18, ALL_HF_MODELS
 from global_utils.deterministic import TRUE
 from global_utils.write_results import write_measurements_and_args_to_json_file
 
@@ -108,9 +108,7 @@ if __name__ == "__main__":
         # DISTRIBUTIONS: [TOP_LAYERS, TWENTY_FIVE_PERCENT, FIFTY_PERCENT],
         APPROACHES: ["baseline", "shift", "mosix"],
         DEFAULT_CACHE_LOCATIONS: ["CPU"],
-        SNAPSHOT_SET_STRINGS: [",".join([MICROSOFT_RESNET_18, MICROSOFT_RESNET_152])],
-        # this line to use all snapshots combined
-        # SNAPSHOT_SET_STRINGS: [",".join(ALL_HF_MODELS)],  # this line to use all snapshots combined
+        SNAPSHOT_SET_STRINGS: [",".join(ALL_HF_MODELS)],  # this line to use all snapshots combined
         # SNAPSHOT_SET_STRINGS: ALL_HF_MODELS, # this line for separate search per model
         NUMS_MODELS: [exp_args.num_models],
         BENCHMARK_LEVELS: ["STEPS_DETAILS"],
