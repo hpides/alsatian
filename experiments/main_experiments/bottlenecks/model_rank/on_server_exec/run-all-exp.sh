c64191e1f5084df61fe6c6c9c1bf6c21d6b40005
Nils Strassenburg <nils.strassenburg@hpi.de>
2025-08-11 15:24:02 +0200

    run_exp script

--- experiments/main_experiments/bottlenecks/model_rank/on_server_exec/run-all-exp.sh
+++ experiments/main_experiments/bottlenecks/model_rank/on_server_exec/run-all-exp.sh
@@ -1,20 +1,20 @@
 #!/bin/sh 
 for i in $(seq 1 5);
 do
-	./run_exp.sh bottleneck_analysis-model-resnet18-items-96-split-None-dataset_type-imagenette 
-	./run_exp.sh bottleneck_analysis-model-resnet18-items-1024-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-resnet18-items-9216-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-resnet18-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
-	./run_exp.sh bottleneck_analysis-model-resnet152-items-96-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-resnet152-items-1024-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-resnet152-items-9216-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-resnet152-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
-	./run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-96-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-1024-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-9216-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
-	./run_exp.sh bottleneck_analysis-model-vit_l_32-items-96-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-vit_l_32-items-1024-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-vit_l_32-items-9216-split-None-dataset_type-imagenette
-	./run_exp.sh bottleneck_analysis-model-vit_l_32-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
+	sh run_exp.sh bottleneck_analysis-model-resnet18-items-96-split-None-dataset_type-imagenette 
+	sh run_exp.sh bottleneck_analysis-model-resnet18-items-1024-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-resnet18-items-9216-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-resnet18-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
+	sh run_exp.sh bottleneck_analysis-model-resnet152-items-96-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-resnet152-items-1024-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-resnet152-items-9216-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-resnet152-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
+	sh run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-96-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-1024-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-9216-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-eff_net_v2_l-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
+	sh run_exp.sh bottleneck_analysis-model-vit_l_32-items-96-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-vit_l_32-items-1024-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-vit_l_32-items-9216-split-None-dataset_type-imagenette
+	sh run_exp.sh bottleneck_analysis-model-vit_l_32-items-1024-split--3-dataset_type-imagenette_preprocessed_ssd
 done
