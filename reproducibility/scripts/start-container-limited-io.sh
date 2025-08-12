310b3bc1581cd9f2d863cdbbb37377a7e339fb5f
Nils Strassenburg <nils.strassenburg@hpi.de>
2025-08-12 14:27:45 +0200

    wip fig 10

--- reproducibility/scripts/start-container-limited-io.sh
+++ reproducibility/scripts/start-container-limited-io.sh
@@ -7,8 +7,8 @@ DOCKER="nils-pytorch"
 docker run -d -p 3318:22 \
            --ipc=host \
            -h $DOCKER \
-           --mount type=bind,source=/home/fgrabl/nils-strassenburg/docker-mounted/mount-ssd,target=/mount-ssd \
-           --mount type=bind,source=/fs/nils-strassenburg/docker-mounted/mount-fs,target=/mount-fs \
+            --mount type=bind,source=/home/fgrabl/nils-strassenburg/docker-mounted/repro-mount-ssd,target=/mount-ssd \
+           --mount type=bind,source=/fs/nils-strassenburg/docker-mounted/repro-mount-fs,target=/mount-fs \
            --gpus device=1 \
            --cpuset-cpus="64-127" \
            --restart always \
