38fbffa44815256679fdeff48ac76bfb4782eda0
Nils Strassenburg <nils.strassenburg@hpi.de>
2025-08-14 00:28:32 +0200

    integrating fig11 into fig 10 script

--- reproducibility/scripts/figure-10-11.sh
+++ reproducibility/scripts/figure-10-11.sh
@@ -34,26 +34,26 @@ for model in resnet18 resnet152; do
   fi
 done
 
-for model in eff_net_v2_l vit_l_32; do
-  mkdir -p "/mount-fs/snapshot-sets/$model"
-  cd "/mount-fs/snapshot-sets/$model" || exit
-  for chunk in TOP_LAYER FIFTY_PERCENT TWENTY_FIVE_PERCENT; do
-    tar_file="${model}_${chunk}.tar"
-    unpack_dir="${model}_${chunk}"
-
-    if [ ! -f "$tar_file" ]; then
-      wget "https://data-engineering-systems.s3.openhpicloud.de/nils-strassenburg/alsatian/snapshot-sets/${tar_file}"
-    else
-      echo "$tar_file already exists, skipping download."
-    fi
-
-    if [ ! -d "$unpack_dir" ]; then
-      tar -xf "$tar_file"
-    else
-      echo "$unpack_dir already exists, skipping extraction."
-    fi
-  done
-done
+#for model in eff_net_v2_l vit_l_32; do
+#  mkdir -p "/mount-fs/snapshot-sets/$model"
+#  cd "/mount-fs/snapshot-sets/$model" || exit
+#  for chunk in TOP_LAYER FIFTY_PERCENT TWENTY_FIVE_PERCENT; do
+#    tar_file="${model}_${chunk}.tar"
+#    unpack_dir="${model}_${chunk}"
+#
+#    if [ ! -f "$tar_file" ]; then
+#      wget "https://data-engineering-systems.s3.openhpicloud.de/nils-strassenburg/alsatian/snapshot-sets/${tar_file}"
+#    else
+#      echo "$tar_file already exists, skipping download."
+#    fi
+#
+#    if [ ! -d "$unpack_dir" ]; then
+#      tar -xf "$tar_file"
+#    else
+#      echo "$unpack_dir already exists, skipping extraction."
+#    fi
+#  done
+#done
 
 
 # Download and extract Imagenette2 dataset
