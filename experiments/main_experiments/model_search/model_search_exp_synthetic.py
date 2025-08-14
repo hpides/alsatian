929c255fbf49a750caa520095a5aa65567463a96
Nils Strassenburg <nils.strassenburg@hpi.de>
2025-08-15 00:25:18 +0200

    debug model store location

--- experiments/main_experiments/model_search/model_search_exp_synthetic.py
+++ experiments/main_experiments/model_search/model_search_exp_synthetic.py
@@ -15,7 +15,6 @@ from experiments.main_experiments.snapshots.synthetic.generate_set import get_ar
 from experiments.main_experiments.snapshots.trained.build_trained_model_store import get_trained_models_and_model_store
 from global_utils.benchmark_util import Benchmarker
 from global_utils.constants import TRAIN, TEST, END_TO_END, DETAILED_TIMES
-from global_utils.file_names import parsable_as_list
 from global_utils.model_names import VISION_MODEL_CHOICES, RESNET_18, RESNET_152, EFF_NET_V2_L, VIT_L_32, BERT
 from model_search.approaches import baseline, mosix, shift
 from model_search.execution.data_handling.data_information import DatasetClass
@@ -99,10 +98,11 @@ def run_model_search(exp_args: ExpArgs):
     if exp_args.trained_snapshots:
         for snapshot in model_snapshots:
             print("BASE EXP SAVE PATH", exp_args.base_snapshot_save_path)
-            snapshot.state_dict_path = snapshot.state_dict_path.replace("/mount-ssd/snapshot-dir", exp_args.base_snapshot_save_path)
+            replace_string = exp_args.base_snapshot_save_path
+            snapshot.state_dict_path = snapshot.state_dict_path.replace(
+                "/mount-ssd/snapshot-dir", replace_string.replace("/modelstore_savepath", ""))
 
     print("model_snapshots: ", model_snapshots[0])
-
 
     if exp_args.num_models > 0:
         assert len(model_snapshots) == exp_args.num_models
