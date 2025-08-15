65eda5e86964237f18a395fd25b16b8bb6c4fac7
Nils Strassenburg <nils.strassenburg@hpi.de>
2025-08-16 00:01:54 +0200

    figure12 wip

--- reproducibility/scripts/figure-12.sh
+++ reproducibility/scripts/figure-12.sh
@@ -30,38 +30,38 @@ sh /mount-ssd/script-execution/fig12/model_resource_info_exp_on_server.sh
 echo "✅ Experiments done"
 echo "results can be found under /mount-fs/results/fig12/"
 
-#mkdir -p /mount-fs/plots/fig12/
-#cd /mount-fs/plots/fig12/
-#
-## Assigning command line arguments to variables
-#repository_url=https://github.com/hpides/alsatian.git
-#script_to_start=experiments/main_experiments/model_search/eval/trained_snapshots/plot_trained_snapshots.py
-#branch=reproducibility
-#python_dir=/home/nils/.virtualenvs/model-search-paper/bin/python
-#repo_name=alsatian
-#
-#rm -rf alsatian
-#git clone $repository_url
-#
-## Change directory to the cloned repository
-#repo_name=$(basename "$repository_url" .git)
-#cd $repo_name
-#
-#git pull
-#git checkout $branch
-#git pull
-#
-## Check if the specified script exists
-#if [ ! -f "$script_to_start" ]; then
-#    echo "Script '$script_to_start' not found in the repository."
-#    exit 1
-#fi
-#
-## Set the PYTHONPATH to include the current directory
-#export PYTHONPATH=$PWD
-#
-## Execute the specified Python script
-#$python_dir $script_to_start
-#
-#echo "✅ Plots done"
-#echo "plots can be found under /mount-fs/plots/fig13"
+mkdir -p /mount-fs/plots/fig12/
+cd /mount-fs/plots/fig12/
+
+# Assigning command line arguments to variables
+repository_url=https://github.com/hpides/alsatian.git
+script_to_start=experiments/side_experiments/model_resource_info/eval/plot-comparison-plots.py
+branch=reproducibility
+python_dir=/home/nils/.virtualenvs/model-search-paper/bin/python
+repo_name=alsatian
+
+rm -rf alsatian
+git clone $repository_url
+
+# Change directory to the cloned repository
+repo_name=$(basename "$repository_url" .git)
+cd $repo_name
+
+git pull
+git checkout $branch
+git pull
+
+# Check if the specified script exists
+if [ ! -f "$script_to_start" ]; then
+    echo "Script '$script_to_start' not found in the repository."
+    exit 1
+fi
+
+# Set the PYTHONPATH to include the current directory
+export PYTHONPATH=$PWD
+
+# Execute the specified Python script
+$python_dir $script_to_start
+
+echo "✅ Plots done"
+echo "plots can be found under /mount-fs/plots/fig12"
